6d1b7ddd9856b9d51ca6ebcbc95c2ab06d63807c
Stephen Molloy <sdmolloy@gmail.com>
2024-08-16 17:41:46 +0200

    Added Sbend type

--- src/elements.h
+++ src/elements.h
@@ -10,9 +10,19 @@ typedef struct {
   float k;
 } Quad;
 
+// SBEND,L=0.20424000,  ANGLE = 0.00388598, K1 = -1.33342000  E1= 0.00000000,  E2= 0.00000000 ; 
+typedef struct {
+  float length;
+  float angle;
+  float K1;
+  float E1;
+  float E2;
+} Sbend;
+
 typedef enum {
   ELETYPE_DRIFT,
   ELETYPE_QUAD,
+  ELETYPE_SBEND,
 } EleType;
 
 typedef struct {
@@ -20,9 +30,9 @@ typedef struct {
   union {
     Drift drift;
     Quad quad;
+    Sbend sbend;
   } as;
 } Element;
 
 #endif // !ELEMENTS_H
-#define ELEMENTS_H
 
