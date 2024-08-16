882ea347db1bc449ce2ce27bbac164ba57401e5c
Stephen Molloy <sdmolloy@gmail.com>
2024-08-16 17:40:27 +0200

    No need for my own SizedString type thanks to STB dyn arrays

--- src/main.c
+++ src/main.c
@@ -16,37 +16,6 @@ typedef struct {
   float value;
 } hash_map;
 
-typedef struct {
-  size_t len;
-  size_t cap;
-  char *data;
-} SizedString;
-
-SizedString new_sized_string(void) {
-  SizedString result = {0};
-  result.cap = 256;
-  result.data = malloc(result.cap * sizeof(char));
-  if (result.data == NULL) result.cap = 0;
-  return result;
-}
-
-void free_sized_string(SizedString s_string) {
-  s_string.cap = 0;
-  s_string.len = 0;
-  free(s_string.data);
-}
-
-int append_to_sized_string(SizedString *ca, char new_element) {
-  while (ca->len >= ca->cap) {
-    ca->cap *= 2;
-    ca->data = realloc(ca->data, ca->cap);
-  }
-  if (ca->data == NULL) return -1;
-  ca->data[ca->len] = new_element;
-  ca->len++;
-  return 1;
-}
-
 static Arena memory_arena = {0};
 
 int main(void) {
